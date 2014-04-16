3f1b934c277d8533cbdc35fc1ac0657082b20cb8
Robert Rozic <rooobertinhooo@gmail.com>
2014-04-16 20:59:39 +0200

    Remove swap script
    Swap is moved to common

--- codinap.mk
+++ codinap.mk
@@ -48,10 +48,3 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
-
-# Storage switch script
-PRODUCT_COPY_FILES += \
-    $(CODINA_PATH)/configs/SwapStorages.sh:system/xbin/SwapStorages.sh
-PRODUCT_PROPERTY_OVERRIDES += \
-     persist.sys.vold.switchablepair=sdcard0,sdcard1 \
-     persist.sys.vold.switchexternal=0
